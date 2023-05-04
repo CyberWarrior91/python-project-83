da2a9d1a07e655f97f2174608cbb77f97cec1c02
Vladislav25 <vladkhleborodov@gmail.com>
2023-05-04 15:33:40 +0300

    fix linter issues

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -18,13 +18,7 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.getenv('SECRET_KEY')
-try:
-    # пытаемся подключиться к базе данных
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    conn = psycopg2.connect(DATABASE_URL)
-except:
-    # в случае сбоя подключения будет выведено сообщение в STDOUT
-    print('Can`t establish connection to database')
+
 
 @app.route('/')
 def main():
@@ -40,6 +34,7 @@ def validation(url):
         errors['wrong_url'] = 'Некорректный URL'
     return errors
 
+
 @app.route('/urls', methods=['GET', 'POST'])
 def handle_urls():
     DATABASE_URL = os.getenv('DATABASE_URL')
@@ -51,18 +46,24 @@ def handle_urls():
             flash('URL обязателен')
             return redirect(url_for('main'), code=302)
         if validators.url(new_url):
-            curr_url = f'{urlparse(new_url).scheme}://{urlparse(new_url).netloc}'
+            curr_url = f'{urlparse(new_url).scheme} \
+            ://{urlparse(new_url).netloc}'
             with conn.cursor() as curs:
                 try:
-                    curs.execute("""SELECT id FROM urls WHERE name=%s""", (curr_url, ))
+                    curs.execute("""SELECT id FROM urls WHERE name=%s""",
+                                 (curr_url, ))
                     url_id = curs.fetchone()[0]
                     flash('Страница уже существует')
                     return redirect(url_for('url_page', id=url_id), code=302)
-                except:
-                    curs.execute("""INSERT INTO urls (name, created_at) VALUES (%s, %s);""", (curr_url, date.today()))
+                except Exception:
+                    curs.execute(
+                        """INSERT INTO urls (name, created_at)
+                        VALUES (%s, %s);""",
+                        (curr_url, date.today()))
                     conn.commit()
             with conn.cursor() as curs:
-                curs.execute("""SELECT id FROM urls WHERE name=%s""", (curr_url, ))
+                curs.execute("""SELECT id FROM urls WHERE name=%s""",
+                             (curr_url, ))
                 url_id = curs.fetchone()[0]
             curs.close()
             conn.close()
@@ -71,8 +72,6 @@ def handle_urls():
         else:
             flash('Некорректный URL')
             return redirect(url_for('main'), code=302)
-
-
     if request.method == 'GET':
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
             curs.execute("""SELECT id, name, created_at FROM urls""")
@@ -88,9 +87,10 @@ def url_page(id):
     conn = psycopg2.connect(DATABASE_URL)
     messages = get_flashed_messages(with_categories=True)
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
-        curs.execute("""SELECT id, name, created_at FROM urls WHERE id=%s""", (id, ))
+        curs.execute(
+            """SELECT id, name, created_at
+            FROM urls WHERE id=%s""", (id, ))
         url = curs.fetchone()
     curs.close()
     conn.close()
     return render_template('single_url.html', messages=messages, url=url)
-
